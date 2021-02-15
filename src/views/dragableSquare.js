3bd34c9a3d99dde514ea4aa63c5780f75d0bd952
Hugo José Bello <hugojose@Hugos-MacBook-Pro.local>
2021-02-15 19:13:30 +0100

    clean

--- src/views/dragableSquare.js
+++ src/views/dragableSquare.js
@@ -1,44 +1,71 @@
 //https://blog.reactnativecoach.com/creating-draggable-component-with-react-native-132d30c27cb0
-import React, {useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Animated, View, StyleSheet, PanResponder, Text} from "react-native";
 
 const DragableSquare = () => {
-    const pan = useRef(new Animated.ValueXY()).current;
-    const opacity = useRef(new Animated.Value(1))
+    const pan = new Animated.ValueXY()
+    const dim = useRef(new Animated.Value(1)).current;
+    //const opacity = new Animated.Value(1)
+    const [showDragable, setShowDragable] = useState(true)
 
-    const panResponder = useRef(
-        PanResponder.create({
-            onMoveShouldSetPanResponder: () => true,
-            onPanResponderGrant: () => {
-                pan.setOffset({
-                    x: pan.x._value,
-                    y: pan.y._value
-                });
-            },
-            onPanResponderMove: Animated.event(
-                [
-                    null,
-                    {dx: pan.x, dy: pan.y}
-                ]
-            ),
-            onPanResponderRelease: (e, gesture) => {
+    const panResponderInit = PanResponder.create({
+        onMoveShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            pan.setOffset({
+                x: pan.x._value,
+                y: pan.y._value
+            });
+        },
+        onPanResponderMove: Animated.event(
+            [
+                null,
+                {dx: pan.x, dy: pan.y}
+            ], {useNativeDriver:false}
+        ),
+        onPanResponderRelease: (e, gesture) => {
+            console.log("xx", dim);
+
+            if (isDropArea(gesture)) {
+                Animated.timing(dim, {
+                    toValue: 0,
+                    duration: 1000
+                }).start(() =>{
+                        console.log("--", dim);
+                        setShowDragable(false);
+                }
+
+                );
+            } else {
                 Animated.spring(pan, {
                     toValue: {x: 0, y: 0},
                     friction: 5
                 }).start();
-            },
-        })
-    ).current;
+            }
+        }
+    })
 
-    return (
-        <Animated.View
-            style={{
-                transform: [{translateX: pan.x}, {translateY: pan.y}]
-            }}
-            {...panResponder.panHandlers}
-        >
-            <View style={styles.circle}/>
-        </Animated.View>
+    const [panResponder, setPanResponder] = useState(panResponderInit)
+
+    useEffect(() => {
+        setPanResponder(panResponderInit)
+    }, [])
+
+    const isDropArea = (gesture) => {
+        return gesture.moveY < 200;
+    }
+
+
+    return (<View style={{width: "20%", alignItems: "center"}}>
+            {showDragable && <Animated.View
+                style={[{
+                    transform: [{translateX: pan.x}, {translateY: pan.y}]
+                }, {opacity:dim}]}
+                {...panResponder.panHandlers}
+            >
+                <View
+                    style={[styles.circle]}/>
+            </Animated.View>}
+        </View>
     );
 }
 
